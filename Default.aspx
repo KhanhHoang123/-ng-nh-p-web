188b0b1d73915a1b259a4fc0014e138e3bc71cab
KhanhHoang <1551010037hoang@ou.edu.vn>
2017-10-16 22:13:16 +0700

    Đăng nhập web

---
+++ Default.aspx
@@ -0,0 +1,40 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+
+    <style>
+        .formclass {
+        padding:20px;
+        margin: 0px auto;
+        background:#fff;
+        width:200px;
+        }
+        h2{
+            text-align:center;
+        }
+        .input
+        {
+            padding:2px;
+            }
+        body{
+            background:#ccc;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div class="formclass">
+        <h2>Đăng nhập</h2>
+        <asp:TextBox ID="txtUser" CssClass="input" placeholder="User Name" runat="server" Width="163px"></asp:TextBox><br /> <br />
+        
+        <asp:TextBox ID="txtPass" CssClass="input" placeholder="Password" TextMode="Password" 
+            runat="server" Width="160px"></asp:TextBox> <br /> <br />
+        <asp:Button ID="btDangnhap" runat="server" Text="Đăng nhập" Height="38px" Width="129px" OnClick="btDangnhap_Click" />
+    </div>
+    </form>
+</body>
+</html>
---
+++ Default.aspx.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Configuration;
+
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        
+    }
+    
+    protected void btDangnhap_Click(object sender, EventArgs e)
+    {
+        //string cnstr = ConfigurationManager.ConnectionStrings["connect"].ConnectionString;
+        //SqlConnection con = new SqlConnection(cnstr);
+        SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["connect"].ToString());
+        con.Open();
+        string query = "SELECT COUNT(*) FROM [login_page].[dbo].[User] where user_name='" + txtUser.Text+"' and pass ='"+txtPass.Text+"'";
+        SqlCommand cmd = new SqlCommand(query, con);
+        string output = cmd.ExecuteScalar().ToString();
+        if (output == "1")
+        {
+            Session["user"] = txtUser.Text;
+            Response.Redirect("~/welcome.aspx");
+        }
+        else
+        {
+            Response.Write("Đăng nhập thất bại");
+        }
+    }
+}
